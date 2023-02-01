3e6fdaf778e327f006a7627989aca5dbde73ce6b
Nicolas CARREZ <nicolas.carrez@gmail.com>
2023-02-01 23:19:00 +0100

    Add list endpoint

--- src/actix_seaorm_api.rs
+++ src/actix_seaorm_api.rs
@@ -1,7 +1,7 @@
 use serde::{Serialize, Deserialize};
 use actix_web as web;
 use sea_orm as orm;
-use sea_orm::{EntityTrait, Iterable, PrimaryKeyToColumn};
+use sea_orm::{EntityTrait, Iterable, PrimaryKeyToColumn, QuerySelect};
 
 
 pub struct ModelApi<Model, ActiveModel>
@@ -21,6 +21,7 @@ where
     pub fn services(cfg: &mut web::web::ServiceConfig) {
         cfg.service(
             web::web::resource("/")
+                .route(web::web::get().to(Self::list_model))
                 .route(web::web::post().to(Self::create_model))
         );
         cfg.service(
@@ -73,15 +74,34 @@ where
             }
         }
     }
+
+    async fn list_model(
+        conn: web::web::Data<orm::DatabaseConnection>,
+    ) -> Result<web::web::Json<ResList<<<Model as sea_orm::ModelTrait>::Entity as sea_orm::EntityTrait>::Model>>, web::Error>{
+        match <Model as orm::ModelTrait>::Entity::find().limit(10).all(conn.get_ref()).await {
+            Err(_) => Err(web::error::ErrorInternalServerError("Internal Error")),
+            Ok(items) => Ok(web::web::Json(ResList{
+                items: items
+            })),
+        }
+    }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
 struct ResId<Model: orm::ModelTrait>
 where
     <<<Model as orm::ModelTrait>::Entity as orm::EntityTrait>::PrimaryKey as orm::PrimaryKeyTrait>::ValueType : Serialize,
-    <<<Model as orm::ModelTrait>::Entity as orm::EntityTrait>::PrimaryKey as orm::PrimaryKeyTrait>::ValueType : for<'a> Deserialize<'a>,
 {
     pub id: <<<Model as orm::ModelTrait>::Entity as orm::EntityTrait>::PrimaryKey as orm::PrimaryKeyTrait>::ValueType,
+}
+
+
+#[derive(Serialize)]
+struct ResList<Model: orm::ModelTrait>
+where
+    Model : Serialize,
+{
+    pub items: Vec<Model>,
 }
 
 fn unset_primary_key<ActiveModel: orm::ActiveModelTrait>(model: &mut ActiveModel) {
